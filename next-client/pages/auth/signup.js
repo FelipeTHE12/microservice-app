9fe1585f3951098b3b7a013b8ee196b36185d0ba
Felipe <felipeoliveirasilva12@outlook.com>
2022-01-25 00:07:05 -0300

    feat(CLIENT): add next inital setup and bootstrap

---
+++ next-client/pages/auth/signup.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+export default () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(email, password);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h1>Sign Up</h1>
+      <div className="form-group">
+        <label>Email</label>
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          type="text"
+          className="form-control"
+        />
+      </div>
+      <div className="form-group">
+        <label>Password</label>
+        <input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          type="password"
+          className="form-control"
+        />
+      </div>
+      <button className="btn btn-primary">SIGN UP !</button>
+    </form>
+  );
+};
