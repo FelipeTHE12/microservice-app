b420aceb6f6dcbc2bb373462fb1abe52575b1c2e
Felipe <felipeoliveirasilva12@outlook.com>
2022-01-27 23:14:44 -0300

    wip(CLIENT): fetching current user on HomePage

--- next-client/pages/index.js
+++ next-client/pages/index.js
@@ -1,3 +1,13 @@
-export default () => {
+import axios from "axios";
+
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
   return <h1>Home page</h1>;
 };
+
+LandingPage.getInitialProps = async () => {
+  const response = await axios.get("/api/users/currentuser").catch(err);
+  return response.data;
+};
+
+export default LandingPage;
