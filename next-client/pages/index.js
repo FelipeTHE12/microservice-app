87c19dcf14a9d094d80256d28cd7158f0631a277
Felipe <felipeoliveirasilva12@outlook.com>
2022-01-28 23:45:50 -0300

    wip(CLIENT): configuring client requests on k8s cluster

--- next-client/pages/index.js
+++ next-client/pages/index.js
@@ -6,8 +6,19 @@ const LandingPage = ({ currentUser }) => {
 };
 
 LandingPage.getInitialProps = async () => {
-  const response = await axios.get("/api/users/currentuser").catch(err);
-  return response.data;
+  if (typeof window === "undefined") {
+    const { data } = await axios
+      .get(
+        "http://ingress-nginx.ingress-nginx.svc.cluster.local/api/users/currentuser"
+      )
+      .catch(err);
+    return data;
+  } else {
+    const { data } = await axios.get("/api/users/currentuser").catch(err);
+    return data;
+  }
+
+  return {};
 };
 
 export default LandingPage;
