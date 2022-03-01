ad258fcd5c09a182fb8b4aadccc4e977430177b8
Felipe <felipeoliveirasilva12@outlook.com>
2022-03-01 12:37:54 -0300

    feat:(nats-test): add testing nats server

---
+++ nats-test/src/publisher.ts
@@ -0,0 +1,21 @@
+import nats from "node-nats-streaming";
+
+console.clear();
+//client
+const stan = nats.connect("ticketing", "123", {
+  url: "http:localhost:4222",
+});
+
+stan.on("connect", () => {
+  console.log("Publisher connected to NATS");
+
+  const data = JSON.stringify({
+    id: "123456",
+    title: "sfaasd",
+    price: 30,
+  });
+
+  stan.publish("ticket:created", data, () => {
+    console.log("Event published");
+  });
+});
