0264f4579572b2c7415fa0be57e6aa20b5c30aae
Felipe <felipeoliveirasilva12@outlook.com>
2022-03-02 12:53:59 -0300

    wip(nats-test): wip on understanding

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,21 +1,36 @@
 import nats, { Message } from "node-nats-streaming";
-import { type } from "os";
+import { randomBytes } from "crypto";
 
 console.clear();
-
-const stan = nats.connect("ticketing", "abc", {
+///client ID
+const stan = nats.connect("ticketing", randomBytes(4).toString("hex"), {
   url: "http:localhost:4222",
 });
 
 stan.on("connect", () => {
   console.log("Listener connected to nats");
 
-  const subscription = stan.subscribe("ticket:created");
+  stan.on("close", () => {
+    console.log("Nats connection closed");
+    process.exit();
+  });
+  const options = stan.subscriptionOptions().setManualAckMode(true);
+
+  const subscription = stan.subscribe(
+    "ticket:created",
+    "listenerQueueGroupTicket",
+    options
+  );
 
   subscription.on("message", (msg: Message) => {
     console.log("Message received");
     const data = msg.getData();
 
     typeof data === "string" && console.log(data);
+
+    msg.ack();
   });
 });
+
+process.on("SIGINT", () => stan.close());
+process.on("SIGTERM", () => stan.close());
