8aa10c1f25589e464aa1e7f49f6b262ec782b935
Felipe <felipeoliveirasilva12@outlook.com>
2022-04-06 17:38:53 -0300

    wip(TICKETS): saving wip

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express";
 import { body } from "express-validator";
 import { requireAuth, validateRequest } from "@12ticketsapp/common";
 import { Ticket } from "../models/ticket";
+import { TicketCreatedPublisher } from "../events/publishers/ticket-created-publisher";
 
 const router = express.Router();
 
@@ -22,15 +23,22 @@ router.post(
   ],
   validateRequest,
   async (req: Request, res: Response) => {
-    const {title, price} = req.body;
+    const { title, price } = req.body;
 
     const ticket = Ticket.build({
       title,
       price,
-      userId: req.currentUser!.id
+      userId: req.currentUser!.id,
     });
 
     await ticket.save();
+
+    new TicketCreatedPublisher(client).publish({
+      id: ticket.id,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId,
+    });
 
     res.status(201).send(ticket);
   }
